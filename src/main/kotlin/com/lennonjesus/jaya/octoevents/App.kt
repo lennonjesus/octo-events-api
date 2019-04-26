7bc94c14991594a504a1534363c5b596a059d847
Lennon Jesus <lennon.jesus@gmail.com>
2019-04-26 11:52:44 -0300

    chore: Adds heroku port

--- src/main/kotlin/com/lennonjesus/jaya/octoevents/App.kt
+++ src/main/kotlin/com/lennonjesus/jaya/octoevents/App.kt
@@ -3,15 +3,18 @@ package com.lennonjesus.jaya.octoevents
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.get
 import io.javalin.apibuilder.ApiBuilder.post
+//import kotlin.text.toIntOrNull
 
 fun main() {
 
     val issueRepository = IssueRepository()
 
+    val port: Int = System.getenv("PORT")?.toIntOrNull() ?: 7000
+
     val app = Javalin.create().apply {
         exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
         error(404) { ctx -> ctx.json("not found") }
-    }.start(7000)
+    }.start(port)
 
 
     app.routes {
